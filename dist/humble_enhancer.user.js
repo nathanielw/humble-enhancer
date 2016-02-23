d6a35dbe16e46b908c83b3f5619f4999ad9c20a6
Nathaniel Watson <nathaniel@nwatson.nz>
2016-02-23 22:14:59 +1300

    Switch stylesheet URL to absolute Github link
    This will allow the CSS to be loaded when the script is installed from
    external sites.
    
    The URL uses the package version to point to a Git tag, which should
    work as long as the tag format is consistent.

--- dist/humble_enhancer.user.js
+++ dist/humble_enhancer.user.js
@@ -23,7 +23,7 @@
 // @grant         GM_addStyle
 // @connect       humblebundle.com
 // @connect       cheapshark.com
-// @resource      style style.css
+// @resource      style https://cdn.rawgit.com/nathanielw/humble-enhancer/v0.1.0/dist/style.css
 // ==/UserScript==
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
