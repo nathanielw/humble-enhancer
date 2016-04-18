a1b0718e896705596748c2da84063ee8ab837570
Nathaniel Watson <nathaniel@nwatson.nz>
2016-04-18 16:22:53 +1200

    Fix Tampermonkey blocking API connections
    Tampermonkey doesn't seem to treat @connect values as including
    subdomains, so the www version for the cheapshark api had to be added
    explicitly.

--- dist/humble_enhancer.user.js
+++ dist/humble_enhancer.user.js
@@ -13,7 +13,7 @@
 // @exclude       https://www.humblebundle.com/books*
 // @exclude       https://www.humblebundle.com/monthly*
 // @exclude       https://www.humblebundle.com/store*
-// @version       0.2.0
+// @version       0.2.1
 // @grant         GM_xmlhttpRequest
 // @grant         GM_getValue
 // @grant         GM_setValue
@@ -23,7 +23,8 @@
 // @grant         GM_addStyle
 // @connect       humblebundle.com
 // @connect       cheapshark.com
-// @resource      style https://cdn.rawgit.com/nathanielw/humble-enhancer/v0.2.0/dist/style.css
+// @connect       www.cheapshark.com
+// @resource      style https://cdn.rawgit.com/nathanielw/humble-enhancer/v0.2.1/dist/style.css
 // ==/UserScript==
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
@@ -658,7 +659,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 var TYPE_VERSION = 'version';
 var ID_VERSION = 'installed';
-var VERSION = '0.2.0';
+var VERSION = '0.2.1';
 
 function updateTasks() {
 	var oldVersion = Cache.getValue(TYPE_VERSION, ID_VERSION);
